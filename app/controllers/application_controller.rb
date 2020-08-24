645fee92a1c35bed3074aa73d4f995e0902069bf
josephinehall <josephinehall@gmail.com>
2020-08-24 13:57:34 +1200

    Add simple token-based API authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  TOKEN = ENV["API_TOKEN"]
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+    end
+  end
 end
